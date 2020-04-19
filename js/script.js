2c0ea4f19dee4334ac28e26fd7e1f6368d21ef0d
Hsu Myat <hsumyatnoenwe@gmail.com>
2020-04-19 14:35:47 +0630

    slider finished

--- js/script.js
+++ js/script.js
@@ -1,35 +1,22 @@
 
 $(document).ready(function(){
-   $(".abt-link").on("click" ,function(){
-     scrolled=scrolled-300;
-        $("#abtme").animate({
-          scrollTop:  scrolled
-     });
-   });
-   
-   	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-	    var target = this.hash;
-	    var $target = $(target);
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-		});
-		window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v6.0'
-        });
-      };
+	$(".blocks.work-vector-info .vector-txt a").click(function(){
+		$(".page-middle-content").addClass("slide-left");
+		$(".second-div").addClass("second-div-left");
+		$(".last-layer-wp .blocks-inner").addClass("show-goback");
+		$("body").addClass("switch-color");
+	});
+	$(".last-layer-wp a").click(function(){
+		$(".page-middle-content").removeClass("slide-left");
+		$(".second-div").removeClass("second-div-left");
+		$(".last-layer-wp .blocks-inner").removeClass("show-goback");
+		$("body").removeClass("switch-color");
+	})
 
-      (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-      fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+	$(".work-slider-wp").slick({
+		slidesToShow: 3,
+		infinite: false
+	})
+	
 });
 
