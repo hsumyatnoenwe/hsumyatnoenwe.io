e81ff377e3d11811de2c976dc55040607055f426
Hsu Myat <hsumyatnoenwe@gmail.com>
2020-04-25 19:30:46 +0630

    js css updated

--- js/script.js
+++ js/script.js
@@ -1,22 +1,80 @@
-
 $(document).ready(function(){
+
+	function calcwidth() {
+		var bigwidth = $(".all-item-wrapper").width();
+		var midwidth = $(".second-div .page-inner-content").width();
+		var ow = $(".blocks.ellipse-info").width();
+
+		var lefteli = $(".blocks.ellipse-info").css('left');
+
+		lefteli = parseInt(lefteli)
+		lefteli = Math.abs(lefteli)
+
+		var sw = (bigwidth - midwidth) / 2;
+		var allw = (ow + sw) - lefteli;
+
+		$(".work-slider-wp .slick-slide").each(function(){
+			if ($(this).offset().left <= allw) {
+				$(".work-place p", this).addClass("color-update");
+			}
+			else {
+				$(".work-place p", this).removeClass("color-update");
+			}
+		})
+	}
+
+	const slider = $(".work-slider-wp").slick({
+  	arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+		infinite: false
+  });
+
 	$(".blocks.work-vector-info .vector-txt a").click(function(){
 		$(".page-middle-content").addClass("slide-left");
 		$(".second-div").addClass("second-div-left");
 		$(".last-layer-wp .blocks-inner").addClass("show-goback");
 		$("body").addClass("switch-color");
+		$(".work-slider-wp .slick-list").removeClass("no-overflow");
+
+		calcwidth();
+
+		setTimeout(function() {
+			calcwidth();
+		}, 800);
 	});
+
 	$(".last-layer-wp a").click(function(){
 		$(".page-middle-content").removeClass("slide-left");
 		$(".second-div").removeClass("second-div-left");
 		$(".last-layer-wp .blocks-inner").removeClass("show-goback");
 		$("body").removeClass("switch-color");
-	})
+		$(".work-slider-wp .slick-list").addClass("no-overflow");
 
-	$(".work-slider-wp").slick({
-		slidesToShow: 3,
-		infinite: false
-	})
-	
+		slider.slick('slickGoTo', 0);
+
+		calcwidth();
+
+		setTimeout(function() {
+			calcwidth();
+		}, 800);
+	});
+
+	slider.on('wheel', (function(e) {
+	  e.preventDefault();
+
+	  if (e.originalEvent.deltaY < 0) {
+	    $(this).slick('slickPrev');
+	  } else {
+	    $(this).slick('slickNext');
+	  }
+	}));
+
+  slider.on('afterChange', function(event, slick, currentSlide){
+  	calcwidth();
+  });
+
+	calcwidth();
+
 });
 
